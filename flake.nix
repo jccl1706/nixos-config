bf529c8bc38f259039d023b4dcec17d498feac44
Julio Cesar <jccl1706@gmail.com>
2025-03-31 15:14:25 +0000

    Update flake.lock and flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,16 @@
   description = "My NixOS Config";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs@{ self, nixpkgs, ...}: {
+  outputs = inputs@{ self, nixpkgs, nixos-hardware, ...}: {
     nixosConfigurations.framework = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      specialArgs = { inherit inputs; };
+      modules = [ 
+        ./configuration.nix 
+        nixos-hardware.nixosModules.framework-13-7040-amd
+      ];
     };
   };
 }

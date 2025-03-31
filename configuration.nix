23e4a99c395961fde8a3e7dfab9bbba328529cf7
Julio Cesar <jccl1706@gmail.com>
2025-03-31 18:26:42 +0000

    new changes

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,9 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];  
+  boot.initrd.systemd.enable = true;
+  boot.plymouth.enable = true;
 
   networking.hostName = "framework";
   networking.networkmanager.enable = true;
@@ -14,6 +17,29 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = with pkgs; [
+    geary
+    epiphany
+    simple-scan
+    totem
+    yelp
+    cheese
+    baobab
+    gnome-tour
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-logs
+    gnome-font-viewer
+    gnome-calendar
+    gnome-clocks
+    gnome-characters
+    gnome-contacts
+    gnome-weather
+  ];
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
 
   users.users.jc = {
      isNormalUser = true;
@@ -27,9 +53,14 @@
     }];
   }];
 
-  environment.systemPackages = with pkgs; [ git neovim ];
+  environment.systemPackages = with pkgs; [ 
+    git 
+    neovim
+    firefox
+  ];
 
   services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "no";
   services.openssh.settings.PasswordAuthentication = true;
   services.openssh.settings.KbdInteractiveAuthentication = false;
 
--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs@{ self, nixpkgs, nixos-hardware, ...}: {
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
     nixosConfigurations.framework = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       specialArgs = { inherit inputs; };
+      system = "x86_64-linux";
       modules = [ 
         ./configuration.nix 
         nixos-hardware.nixosModules.framework-13-7040-amd
